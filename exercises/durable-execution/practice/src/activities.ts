55d6411cad7704be3797a9b32b8f082ade09d3a1
Jeff Romine <jeff@nightheron.dev>
2025-05-08 22:16:50 -0600

    Exercises

--- exercises/durable-execution/practice/src/activities.ts
+++ exercises/durable-execution/practice/src/activities.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 // TODO import everything from the @temporalio/activity package as activity
+import * as activity from '@temporalio/activity';
 
 import { TranslationActivityInput, TranslationActivityOutput } from './shared';
 
 export async function translateTerm(input: TranslationActivityInput): Promise<TranslationActivityOutput> {
+  const context = activity.Context.current();
   // TODO Define an Activity logger
-
+  const log = context.log;
   // TODO log Activity invocation, at the Info level, and include the term being
+  log.info(`Activity invoked with input: ${JSON.stringify(input, null, 2)}`);
   //      translated and the language code as name-value pairs
 
   const lang = encodeURIComponent(input.languageCode);
@@ -19,16 +22,19 @@ export async function translateTerm(input: TranslationActivityInput): Promise<Tr
     const response = await axios.get(url);
     const content = response.data;
     // TODO  use the Debug log level to log the successful translation and include the
+    log.debug(`Translation successful: ${JSON.stringify(content, null, 2)}`);
     //       translated term as a name-value pair
     return { translation: content };
   } catch (error: any) {
     if (error.response) {
       // TODO  use the Error log level to log the failed response and include the
+      log.error(`Translation failed: ${JSON.stringify(error.response, null, 2)}`);
       //       response status and response data using error.response.status
       //       and error.response.data
       throw new Error(`HTTP Error ${error.response.status}: ${error.response.data}`);
     } else if (error.request) {
       // TODO  use the Error log level to log the failed request and include the
+      log.error(`Request failed: ${JSON.stringify(error.request, null, 2)}`);
       //       value of error.request
       throw new Error(`Request error:  ${error.request}`);
     }
--- exercises/durable-execution/practice/src/workflows.ts
+++ exercises/durable-execution/practice/src/workflows.ts
@@ -1,5 +1,5 @@
 // TODO import the sleep and log packages here by adding it to the list.
-import { proxyActivities } from '@temporalio/workflow';
+import { proxyActivities, log } from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 import { TranslationWorkflowInput, TranslationWorkflowOutput } from './shared';
@@ -11,7 +11,7 @@ const { translateTerm } = proxyActivities<typeof activities>({
 export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): Promise<TranslationWorkflowOutput> {
   // TODO Log, at the Info level, when the Workflow function is invoked
   //      and be sure to include the name passed as input
-
+  log.info(`Workflow invoked with input: ${JSON.stringify(input, null, 2)}`);
   const helloInput = {
     term: 'Hello',
     languageCode: input.languageCode,
@@ -19,11 +19,14 @@ export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): 
 
   // TODO Log, at the Debug level, a message about the Activity to be executed,
   //      be sure to include the language code passed as input
+  log.debug(`Calling translateTerm with: ${JSON.stringify(helloInput)}`);
   const helloResult = await translateTerm(helloInput);
   const helloMessage = `${helloResult.translation}, ${input.name}`;
 
   // TODO: (Part C): log a message at the Debug level and then start a Timer for 10 seconds
-
+  log.debug('Starting a timer for 10 seconds');
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  log.info('Timer expired');
   const goodbyeInput = {
     term: 'Goodbye',
     languageCode: input.languageCode,
@@ -31,7 +34,9 @@ export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): 
 
   // TODO Log, at the Debug level, a message about the Activity to be executed,
   //      be sure to include the language code passed as input
+  log.debug(`Calling translateTerm with: ${JSON.stringify(goodbyeInput)}`);
   const goodbyeResult = await translateTerm(goodbyeInput);
+
   const goodbyeMessage = `${goodbyeResult.translation}, ${input.name}`;
 
   return { helloMessage, goodbyeMessage };
